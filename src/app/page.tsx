d00c36c18ea844e04a36e679ff89908e0c715d41
Akshay <90892081+aksh-mon@users.noreply.github.com>
2025-07-16 22:39:34 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import App from "../app/app/page";
 
 export default function Home() {
   return (
-    <div className="border-[2rem] border-blue-300">
+    <div className="border-[.5rem] border-blue-300">
      <App/> 
     </div>
   );
