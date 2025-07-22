864e6cea7975714fc5fec911d873be5ab3566020
Akshay <90892081+aksh-mon@users.noreply.github.com>
2025-07-22 20:30:31 +0530

    Update page.tsx

--- src/app/footnote/page.tsx
+++ src/app/footnote/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <footer className='bg-white text-black text-center py-3'>
-        &copy; aksh-mon; 2025    
+        &copy; aksh-mon-2025    
     </footer>
   )
 }
