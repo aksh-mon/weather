0cfddd7d82d4a11cc738ae1fc3d48b1f0a12b781
Akshay <90892081+aksh-mon@users.noreply.github.com>
2025-08-08 20:41:25 +0530

    Update page.tsx

--- src/app/flappyBird/page.tsx
+++ src/app/flappyBird/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-"use client";
 
+"use client";
 import { Rotate3DIcon } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
