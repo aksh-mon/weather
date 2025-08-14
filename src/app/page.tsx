e797a04305a232c58e2a41101c6854ccc3b40eeb
Akshay <90892081+aksh-mon@users.noreply.github.com>
2025-08-14 09:56:20 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       {/* HEADER */}
       <header className="bg-[#fff] w-[99%] mx-auto  h-[10vh] border-b-2 border-b-cyan-700 fixed left-0 right-0 top-0 z-[999] flex justify-center items-center">
         <button onClick={() => setIsSidebarOpen(true)}>
-          <Menu className=" w-[2rem] h-[2rem]  ml-3" />
+          <Menu className=" w-[2rem] h-[2rem]  ml-3 bg-black text-black" color="black"/>
         </button>
         <div className="flex items-center w-full justify-center">
           <div className="bg-green-300 w-[3rem] md:w-[5rem] h-[1rem]  border-s-[40px] border-s-amber-700 rounded-s-full"></div>
@@ -105,7 +105,7 @@ export default function Home() {
             </div>
 
             {/* Error Message */}
-            <h1 className='mt-[-5%] text-2xl text-black' style={{fontFamily: 'Nabla'}}>404</h1>
+            <h1 className='mt-[-15%] text-2xl text-black' style={{fontFamily: 'Nabla'}}>404</h1>
             <h1 className={"text-2xl font-bold mt-1 "}>Page Not Available</h1>
             <p className="text-gray-400 text-center mt-1">
               The page you requested is not available. Please try again later.
